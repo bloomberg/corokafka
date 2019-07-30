1d9f95040a2bc7a6e13ec035adfa1269a2e1e7fc
Alexander Popov <apopov49@bloomberg.net>
2019-07-30 13:31:22 -0400

    Cleans up kafka files

--- corokafka/tests/jenkins/kafka/run.sh
+++ corokafka/tests/jenkins/kafka/run.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
-
-sudo su -c "/home/ioitm/kafka_2.11-1.1.0/bin/zookeeper-server-start.sh -daemon /home/ioitm/kafka_2.11-1.1.0/config/zookeeper.properties"
+/opt/bb/share/kafka-2.2.0/zookeeper-server-start.sh -daemon /opt/bb/share/kafka-2.2.0/../config/zookeeper.properties
 echo zookeeper up
 
-sudo su -c "/home/ioitm/kafka_2.11-1.1.0/bin/kafka-server-start.sh -daemon /home/ioitm/kafka_2.11-1.1.0/config/server.properties"
+/opt/bb/share/kafka-2.2.0/kafka-server-start.sh -daemon /opt/bb/share/kafka-2.2.0/../config/server.properties
 echo server up
 
 sleep 5
 
 for i in {1..30}
 do
-/home/ioitm/kafka_2.11-1.1.0/bin/kafka-topics.sh --create --topic kfk$i --zookeeper 127.0.0.1:2181 --partitions 4 --replication-factor 1
+/opt/bb/share/kafka-2.2.0/kafka-topics.sh --create --topic kfk$i --zookeeper 127.0.0.1:2181 --partitions 4 --replication-factor 1
 done
 
-#/home/ioitm/kafka_2.11-1.1.0/bin/kafka-topics.sh --create --topic my-kafka-topic --zookeeper 127.0.0.1:2181 --partitions 4 --replication-factor 1
-
-#exec /home/ioitm/kio.t.linux_64.tsk
 exec /corokafka/build/tests/CoroKafkaTests.Linux
--- corokafka/tests/jenkins/run_tests.sh
+++ corokafka/tests/jenkins/run_tests.sh
@@ -11,7 +11,5 @@ chcon -Rt svirt_sandbox_file_t $ROOTDIR || echo "chcon error"
 # build and run builder docker container
 docker build -t corokafka-console ./corokafka/tests/jenkins/kafka
 docker run --rm -v $ROOTDIR/:/corokafka:rw corokafka-console
-#docker pull artprod.dev.bloomberg.com/bb-etn/nano/builder:latest
-#docker run --rm -v $tdir/:/nano:rw artprod.dev.bloomberg.com/bb-etn/nano/builder:latest
 
 exit 0
