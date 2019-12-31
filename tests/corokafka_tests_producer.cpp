8e5161751a29166750ff8c4330d237c47bec4d85
Alexander Damian <adamian@bloomberg.net>
2019-12-31 10:10:47 -0500

    more work

--- tests/corokafka_tests_producer.cpp
+++ tests/corokafka_tests_producer.cpp
@@ -1,2 +1,27 @@
+#include <corokafka/corokafka.h>
+#include <gtest/gtest.h>
+#include <corokafka_tests_topics.h>
 
+namespace Bloomberg {
+namespace corokafka {
+namespace tests {
+
+void makeConnectorWithMissingBroker() {
+    ck::ProducerConfiguration config(topic1Str, {}, {}); //use all defaults
+    ck::ConfigurationBuilder builder;
+    builder(config);
+    ck::Connector connector(std::move(builder));
+}
+
+TEST(Producer, MissingBrokerList)
+{
+    ck::ProducerConfiguration config(topic1Str, {}, {}); //use all defaults
+    ck::ConfigurationBuilder builder;
+    builder(config);
+    ASSERT(ck::Connector connector(std::move(builder));
+}
+
+}
+}
+}
 
