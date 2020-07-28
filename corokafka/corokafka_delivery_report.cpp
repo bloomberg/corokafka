4798d63d30de49cf6c7ed6ebb3b59764d53ac5ba
Alexander Damian <adamian@bloomberg.net>
2020-07-28 13:23:23 -0400

    Added latency and status support in the DeliveryReport class
    Signed-off-by: Alexander Damian <adamian@bloomberg.net>

--- corokafka/corokafka_delivery_report.cpp
+++ corokafka/corokafka_delivery_report.cpp
@@ -23,21 +23,15 @@ namespace corokafka {
 //====================================================================================
 //                               DELIVERY REPORT
 //====================================================================================
-
-DeliveryReport::DeliveryReport(cppkafka::TopicPartition topicPartition,
-                               size_t numBytes,
-                               cppkafka::Error error,
-                               const void * opaque) :
-    _topicPartition(std::move(topicPartition)),
-    _numBytes(numBytes),
-    _error(std::move(error)),
-    _opaque(opaque)
-{
-}
-
 const cppkafka::TopicPartition& DeliveryReport::getTopicPartition() const
 {
     return _topicPartition;
+}
+
+DeliveryReport& DeliveryReport::topicPartition(cppkafka::TopicPartition topicPartition)
+{
+    _topicPartition = std::move(topicPartition);
+    return *this;
 }
 
 size_t DeliveryReport::getNumBytesWritten() const
@@ -45,14 +39,64 @@ size_t DeliveryReport::getNumBytesWritten() const
     return _numBytes;
 }
 
+DeliveryReport& DeliveryReport::numBytesWritten(size_t bytes)
+{
+    _numBytes = bytes;
+    return *this;
+}
+
 const cppkafka::Error& DeliveryReport::getError() const
 {
     return _error;
 }
 
+DeliveryReport& DeliveryReport::error(cppkafka::Error error)
+{
+    _error = std::move(error);
+    return *this;
+}
+
 void* DeliveryReport::getOpaque() const
 {
     return const_cast<void*>(_opaque);
+}
+
+DeliveryReport& DeliveryReport::opaque(const void* opaque)
+{
+    _opaque = opaque;
+    return *this;
+}
+
+#if (RD_KAFKA_VERSION >= RD_KAFKA_MESSAGE_STATUS_SUPPORT_VERSION)
+rd_kafka_msg_status_t DeliveryReport::getStatus() const
+{
+    return _status;
+}
+
+DeliveryReport& DeliveryReport::status(rd_kafka_msg_status_t status)
+{
+    _status = status;
+    return *this;
+}
+#endif
+
+#if RD_KAFKA_VERSION >= RD_KAFKA_MESSAGE_LATENCY_SUPPORT_VERSION
+std::chrono::microseconds DeliveryReport::getLatency() const
+{
+    return _latency;
+}
+
+DeliveryReport& DeliveryReport::latency(std::chrono::microseconds latency)
+{
+    _latency = std::move(latency);
+    return *this;
+}
+#endif
+
+std::ostream& operator<<(std::ostream& output, const DeliveryReport& dr)
+{
+    output << dr.toString();
+    return output;
 }
 
 std::string DeliveryReport::toString() const
@@ -66,6 +110,12 @@ std::string DeliveryReport::toString() const
         } else {
             json.tag("numBytes", _numBytes);
         }
+#if (RD_KAFKA_VERSION >= RD_KAFKA_MESSAGE_STATUS_SUPPORT_VERSION)
+        json.tag("status", kafkaStatusToString(_status));
+#endif
+#if RD_KAFKA_VERSION >= RD_KAFKA_MESSAGE_LATENCY_SUPPORT_VERSION
+        json.tag("latencyUs", _latency.count());
+#endif
         if (_opaque) {
             json.tag("opaque", _opaque);
         }
@@ -74,10 +124,18 @@ std::string DeliveryReport::toString() const
     return oss.str();
 }
 
-std::ostream& operator<<(std::ostream& output, const DeliveryReport& dr)
+const char* DeliveryReport::kafkaStatusToString(rd_kafka_msg_status_t status)
 {
-    output << dr.toString();
-    return output;
+    switch (status) {
+        case RD_KAFKA_MSG_STATUS_NOT_PERSISTED:
+            return "NotPersisted";
+        case RD_KAFKA_MSG_STATUS_POSSIBLY_PERSISTED:
+            return "PossiblyPersisted";
+        case RD_KAFKA_MSG_STATUS_PERSISTED:
+            return "Persisted";
+        default:
+            return "Unknown";
+    }
 }
   
 }
