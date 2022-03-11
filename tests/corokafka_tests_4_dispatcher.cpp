0ef8e7e7aaa480be30307af5026722f31a158268
Adam M. Rosenzweig <arosenzweig3@bloomberg.net>
2022-03-11 11:16:36 -0500

    Make Dispatcher tear-down an environment
    Signed-off-by: Adam M. Rosenzweig <arosenzweig3@bloomberg.net>

--- tests/corokafka_tests_4_dispatcher.cpp
+++ tests/corokafka_tests_4_dispatcher.cpp
@@ -1,15 +1,20 @@
 #include <corokafka_tests_callbacks.h>
 #include <gtest/gtest.h>
 
-namespace Bloomberg {
-namespace corokafka {
-namespace tests {
-
-TEST(Quantum, Drain)
+namespace Bloomberg::corokafka::tests
 {
-    //This test must run last
-    dispatcher().drain();
-    dispatcher().terminate();
-}
 
-}}}
+class QuantumEnvironment : public ::testing::Environment
+{
+    public:
+        ~QuantumEnvironment() override{};
+
+        void TearDown() override { dispatcher().drain();
+            dispatcher().terminate();
+        }
+};  // class QuantumEnvironment
+
+::testing::Environment* const quantumEnvironment =
+        testing::AddGlobalTestEnvironment(new QuantumEnvironment);
+
+}    // namespace Bloomberg::corokafka::tests
